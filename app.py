83e6674b4479a348fbdb1ba0b9c9ac722b4197b4
PLABAN NAYAK <plaban1981@users.noreply.github.com>
2022-11-16 20:23:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,36 +7,49 @@ import os
 import json
 import pydub
 #
-st.set_page_config(page_title="Detect the language of the audio file App", layout="centered")
-st.image("image.jpg", caption='Detect the language of the audio file')
-# page header
-#st.title(f"English Text to Hindi Translation App")
-with st.form("Prediction_form"):
-   uploadFile = st.file_uploader(label = "Please upload your file ",type=["mp3"])
-   if uploadFile is not None:
-       audio = pydub.AudioSegment.from_mp3(uploadFile)
-       file_type = 'mp3'
-       #filename = uploadFile.name+".mp3"
-       audio.export(uploadFile.name, format="mp3")
-       audio1 = open(uploadFile.name, "rb")
-       audio_bytes = audio1.read() #reading the file
-       st.audio(audio_bytes, format='audio/ogg') #displaying the audio
-   #
-   submit = st.form_submit_button("Detect Language")
-   #
-   if submit:
-        model = whisper.load_model("large").to('cpu')
-        # load audio and pad/trim it to fit 30 seconds
-        audio = whisper.load_audio(uploadFile.name)
-        audio = whisper.pad_or_trim(audio)
-        # make log-Mel spectrogram and move to the same device as the model
-        mel = whisper.log_mel_spectrogram(audio).to(model.device)
-        # detect the spoken language
-        _, probs = model.detect_language(mel)    
-        # decode the audio
-        options = whisper.DecodingOptions(fp16 = False)
-        result = whisper.decode(model, mel, options)
-        languages = {"af_za": "Afrikaans", "am_et": "Amharic", 
+# Init is ran on server startup
+# Load your model to GPU as a global variable here using the variable name "model"
+def init():
+    global model
+    
+    device = 0 if torch.cuda.is_available() else -1
+    model = whisper.load_model("large").to(device)
+#
+# Inference is ran for every server call
+# Reference your preloaded global model variable here.
+def inference(model_inputs:dict) -> dict:
+    global model
+#
+    st.set_page_config(page_title="Detect the language of the audio file App", layout="centered")
+    st.image("image.jpg", caption='Detect the language of the audio file')
+    # page header
+    #st.title(f"English Text to Hindi Translation App")
+    with st.form("Prediction_form"):
+        uploadFile = st.file_uploader(label = "Please upload your file ",type=["mp3"])
+        if uploadFile is not None:
+            audio = pydub.AudioSegment.from_mp3(uploadFile)
+            file_type = 'mp3'
+            #filename = uploadFile.name+".mp3"
+            audio.export(uploadFile.name, format="mp3")
+            audio1 = open(uploadFile.name, "rb")
+            audio_bytes = audio1.read() #reading the file
+            st.audio(audio_bytes, format='audio/ogg') #displaying the audio
+        #
+        submit = st.form_submit_button("Detect Language")
+        #
+        if submit:
+            model = whisper.load_model("large").to('cpu')
+            # load audio and pad/trim it to fit 30 seconds
+            audio = whisper.load_audio(uploadFile.name)
+            audio = whisper.pad_or_trim(audio)
+            # make log-Mel spectrogram and move to the same device as the model
+            mel = whisper.log_mel_spectrogram(audio).to(model.device)
+            # detect the spoken language
+            _, probs = model.detect_language(mel)    
+            # decode the audio
+            options = whisper.DecodingOptions(fp16 = False)
+            result = whisper.decode(model, mel, options)
+            languages = {"af_za": "Afrikaans", "am_et": "Amharic", 
                    "ar_eg": "Arabic", "as_in": "Assamese", 
                    "az_az": "Azerbaijani", "be_by": "Belarusian", 
                    "bg_bg": "Bulgarian", "bn_in": "Bengali", 
@@ -71,11 +84,12 @@ with st.form("Prediction_form"):
                    "sd_in": "Sindhi", "sk_sk": "Slovak", 
                    "sl_si": "Slovenian", "sn_zw": "Shona", "so_so": "Somali", "sr_rs": "Serbian", "sv_se": "Swedish", "sw_ke": "Swahili", "ta_in": "Tamil", "te_in": "Telugu", "tg_tj": "Tajik", "th_th": "Thai", "tr_tr": "Turkish", "uk_ua": "Ukrainian", 
                    "ur_pk": "Urdu", "uz_uz": "Uzbek", "vi_vn": "Vietnamese", "yo_ng": "Yoruba"}
-        decode_language = {k.split("_")[0]: v for k,v in languages.items()}
-        language = decode_language[result.language]
-        text = result.text
-        out = f"Language Detected: {language} Text: {text}"
-        # output header
-        st.header("Summary Gnereated")
-        # output results
-        st.success(f"Translated Text : {out}")
+            decode_language = {k.split("_")[0]: v for k,v in languages.items()}
+            language = decode_language[result.language]
+            text = result.text
+            out = f"Language Detected: {language} Text: {text}"
+            # output header
+            st.header("Summary Gnereated")
+            # output results
+            st.success(f"Translated Text : {out}")
+    return out
